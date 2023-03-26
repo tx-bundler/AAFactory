07a7754083638f6efe18f50cd32be94709ab0ee9
FurkanSezal <fsezal46@gmail.com>
2023-03-26 03:27:19 +0300

    Account deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,46 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import "@matterlabs/hardhat-zksync-chai-matchers";
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-module.exports = {
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+      }
+    : {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: process.env.GOERLI_RPC_URL, // e.g. alchemy url
+        zksync: true,
+        verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
+      };
+
+const config:HardhatUserConfig = {
   zksolc: {
-    version: "1.3.5",
-    compilerSource: "binary",
-    settings: {},
+    version: '1.3.6',
+    compilerSource: 'binary',
+    settings: {
+      isSystem: true,
+    },
   },
+
   defaultNetwork: "zkSyncTestnet",
 
   networks: {
-    zkSyncTestnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-      zksync: true,
+    hardhat: {
+        // @ts-ignore
+        zksync: true
     },
+    zkSyncTestnet,
   },
   solidity: {
-    version: "0.8.17",
+      version: "0.8.16",
   },
 };
+
+export default config;
